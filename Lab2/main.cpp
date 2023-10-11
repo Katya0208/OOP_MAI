26cc4f991010ccc8acfc30a0f9e449c564e5fbf0
Katya Kolomyttseva <68647915+Katya0208@users.noreply.github.com>
2023-10-11 23:15:02 +0300

    Update main.cpp

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -3,19 +3,42 @@
 #include "binary.hpp"
 using namespace std;
 
+#define GREEN_COLOR "\x1b[32m"
+
+// Функция для ввода двоичного числа с клавиатуры
+Binary InputBinaryNumber() {
+  string binaryInput;
+
+  cin >> binaryInput;
+
+  // Парсим строку и создаем объект Binary
+  unsigned int length = binaryInput.length();
+  unsigned char* binaryArray = new unsigned char[length];
+  for (int i = 0; i < length; i++) {
+    binaryArray[i] =
+        binaryInput[i] - '0';  // Преобразуем символы '0' и '1' в числа
+  }
+
+  Binary binaryNumber(length, binaryArray);
+  delete[] binaryArray;
+  return binaryNumber;
+}
+
 int main() {
-  unsigned char number1[] = {1, 1, 1};
-  unsigned char number2[] = {1, 1, 0, 1};
+  cout << "Введите двоичное число N1:" << endl;
+  Binary N1 = InputBinaryNumber();
 
-  Binary N1(sizeof(number1) / sizeof(number1[0]), number1);
-  Binary N2(sizeof(number2) / sizeof(number2[0]), number2);
-
+  cout << "Введите двоичное число N2:" << endl;
+  Binary N2 = InputBinaryNumber();
+  printf(GREEN_COLOR);
+  cout << endl;
   std::cout << "Содержимое массива N1: ";
   N1.Print();
 
   std::cout << "Содержимое массива N2: ";
   N2.Print();
 
+  cout << "Проверка на равенство: ";
   if (N1 == N2) {
     std::cout << "Двоичные числа равны!" << std::endl;
   } else {
@@ -28,13 +51,20 @@ int main() {
   std::cout << "Сумма N1 и N2: ";
   sum.Print();
 
-  std::cout << "Разница N1 и N2: ";
+  std::cout << "Разность N1 и N2: ";
   diff.Print();
-  if (N1 < N2) {
-    cout << "Да, первое число меньше второго!" << endl;
-  }
+  cout << "число1 > число2? ";
   if (N1 > N2) {
-    cout << "Да, первое число больше второго!" << endl;
+    printf(GREEN_COLOR);
+    cout << "Да" << endl;
+  } else {
+    cout << "Нет" << endl;
+  }
+  cout << "число1 < число2? ";
+  if (N1 < N2) {
+    cout << "Да" << endl;
+  } else {
+    cout << "Нет" << endl;
   }
 
   return 0;
